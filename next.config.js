a156759bcb1de4a8af1fbb2d215b360667d6b72a
Mert18 <mertuygur02@gmail.com>
2022-03-29 00:20:19 +0300

    markdown

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    env: {
-        mongodburl: "mongodb+srv://hachiko2k:hachiko2k@portfolio-data.wii6t.mongodb.net/portfolio-data?retryWrites=true&w=majority",
-        SENDGRID_API_KEY: "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"
-    }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,43 @@
-import { Fragment } from "react";
-import Hero from '../components/home-page/hero';
-import HomeContent from '../components/home-page/home-content';
-import { getFeaturedPosts } from '../lib/posts-util.js';
+import styles from "../styles/Home.module.css";
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
 
-
-function HomePage(props) {
-
-    return (
-        <Fragment>
-            <Hero />
-            <HomeContent posts={props.posts} />
-        </Fragment>
-    );
+export default function Home({ posts }) {
+  return (
+    <div className={styles.container}>
+      <h1>zaten</h1>
+      <ul>
+        {posts.map(({ slug, frontmatter }) => (
+          <li key={frontmatter.title}>
+            <Link href={`/blog/${slug}`}>
+              <a>
+                <h1 className="p-4">{frontmatter.title}</h1>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export function getStaticProps() {
-    const featuredPosts = getFeaturedPosts();
+export async function getStaticProps() {
+  // Get all our posts
+  const files = fs.readdirSync("posts");
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const { data: frontmatter } = matter(readFile);
+
     return {
-        props: {
-            posts: featuredPosts
-        }
-    }
+      slug,
+      frontmatter,
+    };
+  });
+  return {
+    props: {
+      posts,
+    },
+  };
 }
-
-export default HomePage;
