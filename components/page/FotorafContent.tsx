09a679a9034d842ddc92f3834629d8ba3f6b8aba
mert.uygur <mert.uygur@ng112.gov.tr>
2025-01-22 13:26:14 +0300

    :bee: fotoraf

---
+++ components/page/FotorafContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContentWrapper from '../ContentWrapper'
+import FotorafList from '../fotoraf/FotorafList'
+
+const FotorafContent = () => {
+  return (
+    <ContentWrapper>
+        <FotorafList />
+    </ContentWrapper>
+  )
+}
+
+export default FotorafContent
