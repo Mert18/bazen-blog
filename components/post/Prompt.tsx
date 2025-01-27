9a7dfdcb48968af9e4def7d28170d90314a40d7f
mert18 <mertuygur02@gmail.com>
2025-01-27 19:27:01 +0300

    better now

--- components/post/Prompt.tsx
+++ components/post/Prompt.tsx
@@ -7,14 +7,14 @@ const Prompt = ({ children }: IRootLayout) => {
     <div className="grid grid-cols-8 w-full mt-2 mb-6">
       <div className="col-start-1 col-end-9 lg:col-start-1 lg:col-end-2 flex items-start justify-start">
         <Image
-          className="border border-accent"
+          className="border border-primary"
           src="/icon/frog.jpg"
           alt="frog"
           width={70}
           height={70}
         />
       </div>
-      <div className="col-start-1 col-end-9 lg:col-start-2 lg:col-end-9 border border-accent1 bg-white2 h-max rounded-sm p-4">
+      <div className="col-start-1 col-end-9 lg:col-start-2 lg:col-end-9 border border-primary bg-primarylighter h-max rounded-sm p-4">
         {children}
       </div>
     </div>
